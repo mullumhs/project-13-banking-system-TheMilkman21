fedc526caf7bd48675db41b8adebb9c0cf0a2a30
TheMilkman21 <140124197+TheMilkman21@users.noreply.github.com>
2024-06-16 23:48:38 +0000

    bank

--- bank.py
+++ bank.py
@@ -13,11 +13,35 @@ class Bank_Manager():
         
     def add_acount(self, name, acc_number, balance):
         for existing_acc in self.accounts:
-            if existing_acc.get_name() == name    
+            if existing_acc.get_name() == name:
+                print("already there an account") 
+                return False
+        account = Account(name, acc_number, balance)
+        self.accounts.append(account)
+        return True
+                   
+    def deposit(self, acc_number, balance, deposit_amount):    
+        for acc_number in self.accounts:
+            if acc_number.get_acc_number() == acc_number:
+                balance + deposit_amount
+                return True
+            else:
+                print("account does not exist")
+                return False
+                
+    def withdraw(self, acc_number, balance, withdraw_amount):
+        for acc_number in self.accounts:
+            if acc_number.get_acc_number() == acc_number:
+                balance - withdraw_amount
+                return True        
+            else:
+                print("account does not exist")
+                return False
         
-        
-        
-        
+    def transfer(self, acc_number, transfer_amount, acc1, acc2):
+        for acc_number in self.accounts:
+            if  acc_number.get_acc_number() == acc_number:
+                    
         
         
         
